1a81c404bb935e32031d4c480150d4b102dbf701
Derik K <derikk@me.com>
2017-08-26 19:16:07 -0400

    Validate fax numbers

--- app/models/business.rb
+++ app/models/business.rb
@@ -2,8 +2,9 @@ class Business < ActiveRecord::Base
 	to_param :name
 	validates :name, uniqueness: true
 	validates :name, :phone, :category, presence: true
-	validates :phone, format: {with: /\A\([0-9]{3}\) [0-9]{3}-[0-9]{4}\z/,
-		message: 'number must be in the form (123) 456-7890'
+	validates :phone, :fax, format: {with: /\A\([0-9]{3}\) [0-9]{3}-[0-9]{4}\z/,
+		message: 'number must be in the form (123) 456-7890',
+		allow_blank: true
 	}
 	belongs_to :category
 end
